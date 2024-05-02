cc00764980ba0086758fc92dd37a5c4d4355514d
Raaee <99899402+Raaee@users.noreply.github.com>
2024-05-02 02:30:16 -0400

    interaction complete

--- Assets/Scripts/Utility/Interactable.cs
+++ Assets/Scripts/Utility/Interactable.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public abstract class Interactable : MonoBehaviour
 {
     public string promptMessage; // Message that displays when in range of an interactable
+    
+    [Header("Debug")]
+    // this is so the interaction does not get called a million times per click
+    [SerializeField] protected bool interacted = false; 
 
     // this will be called by player
     public void BaseInteract() {
+        if (interacted) return;
         Interact();
+        interacted = true;
     }
     protected virtual void Interact() {
         // this will be overridden
