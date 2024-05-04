12d891b5d7aefd1768c8e1ec740009c1dfd6a2b8
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-05-03 20:18:26 -0400

    looking over interaction code

--- Assets/Scripts/Utility/InteractionSystem.cs
+++ Assets/Scripts/Utility/InteractionSystem.cs
@@ -7,32 +7,47 @@ using TMPro;
 /// </summary>
 public class InteractionSystem : MonoBehaviour
 {
-    private RaycastHit2D hit;
+    private RaycastHit2D raycastHit;
     private InputControls input;
     private Interactable lastInteractable;
+    [SerializeField] [Range(0.01f, 4f)]private float interactRadius = 1.5f;
     private void Start() {
         input = GetComponent<InputControls>();
     }
     private void Update() {
-        Vector2 currPos = transform.position;
-        hit = Physics2D.CircleCast(currPos, 1.5f, currPos);
-        if (hit) {
-            Interactable potentialInteractable = hit.collider.GetComponent<Interactable>();
-            if (potentialInteractable) {
-                lastInteractable = potentialInteractable;
-                if (!potentialInteractable.Interacted) {
-                    potentialInteractable.EnablePromptMessage();
-                }
-                if (input.interact.IsPressed()) {
-                    potentialInteractable.BaseInteract();
-                }
+       
+        raycastHit = Physics2D.CircleCast(transform.position, 1.5f, transform.position);
+        if (raycastHit) {
+            Interactable potentialInteractable = raycastHit.collider.GetComponent<Interactable>();
+            if (potentialInteractable == null) return;
+           
+            lastInteractable = potentialInteractable;
+            if (!potentialInteractable.Interacted) {
+                potentialInteractable.EnablePromptMessage();
             }
+            if (input.interact.IsPressed()) { //this should be the initial if statement. if you press interact, do a raycast...
+                potentialInteractable.BaseInteract();
+            }
+            
         }
-        else {
-            if (lastInteractable) {
+        else 
+        {
+            if (lastInteractable) 
+            {
                 lastInteractable.DisablePromptMessage();
             }
         }
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Color gizmoColor = Color.yellow;
+        gizmoColor.a = 0.2f;
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawSphere(transform.position, interactRadius);
     }
 
 }
