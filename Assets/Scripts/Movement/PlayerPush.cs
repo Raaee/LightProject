2f5c0aca12dc5600a45133be69171dbc7c751aa7
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-05-06 20:53:07 -0400

    refactor playmer movemnt and playerpush

--- Assets/Scripts/Movement/PlayerPush.cs
+++ Assets/Scripts/Movement/PlayerPush.cs
@@ -10,55 +10,67 @@ using Vector2 = UnityEngine.Vector2;
 public class PlayerPush : MonoBehaviour
 {
 
-    [SerializeField] private Rigidbody2D box;
+     private Rigidbody2D box;
     [SerializeField] private Transform lowestPoint;
     [SerializeField] private Transform highestPoint;
+    [SerializeField] private bool isHorizontal = true;
 
-
+    private void Awake()
+    {
+        box = GetComponent<Rigidbody2D>();
+    }
     //Molvement of the box when the player is pushing it
     void Update()
     {
-        if (!box)
-        {
-            Debug.Log("No box");
-            return;
-        }
 
+        if (isHorizontal == true)
+            DoHorizontal();
+        else
+            DoVertical();
+    }
+
+    private void DoHorizontal()
+    {
+        box.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+       
         // moves the box to the right and freezes the y position
         if (box.transform.position.x < lowestPoint.position.x)
         {
-            box.transform.position = new Vector2(lowestPoint.position.x, box.position.y);
-            box.constraints = RigidbodyConstraints2D.FreezePositionY;
-            box.constraints = RigidbodyConstraints2D.FreezeRotation;
+            box.transform.position = new Vector2(lowestPoint.position.x, box.position.y);          
         }
         // moves the box to the left and freezes the y position
         if (box.transform.position.x > highestPoint.position.x)
         {
             box.transform.position = new Vector2(highestPoint.position.x, box.position.y);
-            box.constraints = RigidbodyConstraints2D.FreezePositionY;
-            box.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
+      
+    }
+
+    private void DoVertical()
+    {
+        box.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
         // Does not allow the box to go below the lowest point 
         if (box.transform.position.y > lowestPoint.position.y)
         {
             box.transform.position = new Vector2(box.position.x, lowestPoint.position.y);
-            Debug.Log("y");
+           
         }
         if (box.transform.position.y < highestPoint.position.y)
         {
             box.transform.position = new Vector2(box.position.x, highestPoint.position.y);
-            Debug.Log("y");
+          
         }
-
     }
 
-
-    public void OnCollisionEnter2D(Collision2D collision)
+    void OnDrawGizmosSelected()
     {
-        if (collision.gameObject.tag == "Box")
+        if (lowestPoint == null || highestPoint == null)
         {
-            box = collision.gameObject.GetComponent<Rigidbody2D>();
+            return;
         }
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(lowestPoint.position, highestPoint.position);
     }
 
 }
