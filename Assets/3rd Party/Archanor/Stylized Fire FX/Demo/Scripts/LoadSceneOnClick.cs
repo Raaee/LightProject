b1da5b6caddc8278c83db1dea77ac999a8ebe24e
Raaee <99899402+Raaee@users.noreply.github.com>
2024-05-27 22:34:45 -0400

    imported vfx

---
+++ Assets/3rd Party/Archanor/Stylized Fire FX/Demo/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StylizedFireFX
+{
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+	
+	public bool GUIHide = false;
+	public bool GUIHide2 = false;
+	public bool GUIHide3 = false;	
+		
+    public void CollectionFlamethrower()  {
+		SceneManager.LoadScene ("CollectionFlamethrower");
+	}
+    public void CollectionFull()  {
+        SceneManager.LoadScene ("CollectionFull");
+	}
+    public void CollectionMissiles()  {
+        SceneManager.LoadScene ("CollectionMissiles");
+	}
+    public void CollectionSmall()  {
+		SceneManager.LoadScene ("CollectionSmall");
+	}
+    public void FX1Fire()  {
+        SceneManager.LoadScene ("FX1Fire");
+	}
+    public void FX1FireFull()  {
+        SceneManager.LoadScene ("FX1FireFull");
+	}
+    public void FX1Flamethrower()  {
+        SceneManager.LoadScene ("FX1Flamethrower");
+	}
+    public void FX1Missiles()  {
+        SceneManager.LoadScene ("FX1Missiles");
+    }
+    public void FX2Fire()  {
+        SceneManager.LoadScene ("FX2Fire");
+    }
+    public void FX2FireFull() {
+        SceneManager.LoadScene("FX2FireFull");
+    }
+    public void FX2Flamethrower() {
+        SceneManager.LoadScene("FX2Flamethrower");
+    }
+    public void FX2Missiles() {
+        SceneManager.LoadScene("FX2Missiles");
+    }
+	
+	void Update ()
+	 {
+ 
+     if(Input.GetKeyDown(KeyCode.L))
+	 {
+         GUIHide = !GUIHide;
+     
+         if (GUIHide)
+		 {
+             GameObject.Find("CanvasFX1").GetComponent<Canvas> ().enabled = false;
+         }
+		 else
+		 {
+             GameObject.Find("CanvasFX1").GetComponent<Canvas> ().enabled = true;
+         }
+     }
+	      if(Input.GetKeyDown(KeyCode.J))
+	 {
+         GUIHide2 = !GUIHide2;
+     
+         if (GUIHide2)
+		 {
+             GameObject.Find("CanvasFX2").GetComponent<Canvas> ().enabled = false;
+         }
+		 else
+		 {
+             GameObject.Find("CanvasFX2").GetComponent<Canvas> ().enabled = true;
+         }
+     }
+		if(Input.GetKeyDown(KeyCode.H))
+	 {
+         GUIHide3 = !GUIHide3;
+     
+         if (GUIHide3)
+		 {
+             GameObject.Find("CanvasMissileSpawn").GetComponent<Canvas> ().enabled = false;
+         }
+		 else
+		 {
+             GameObject.Find("CanvasMissileSpawn").GetComponent<Canvas> ().enabled = true;
+         }
+     }
+	}
+}
+}
---
+++ Assets/3rd Party/Archanor/Stylized Fire FX/Readme.txt
@@ -0,0 +1,34 @@
+Stylized Fire FX
+---------------
+
+Thanks for buying Stylized Fire FX! In the Prefabs folder you'll find a variety of fire effects in different sizes.
+
+You can simply drag and drop prefabs into the scene, or instantiate them with a script.
+
+You are free to reference or re-purpose any of the included scripts for your games or projects.
+
+---------------
+Scaling Effects
+---------------
+
+You can scale the effects in Transform, but note that Gravity settings in the particle systems may need to be adjusted depending on how much you scale the effect. Any Audio Sources and Point Lights attached to the effect will also have to be adjusted manually.
+
+---------------
+Upgrade to URP
+---------------
+
+To upgrade to URP, import the package in the Upgrade folder.
+
+For Soft Particles to work in URP, you'll need to enable Depth Texture in the same quality setting menu.
+
+You can find the SRP settings in 'Edit -> Quality -> Rendering'. Double-click the profile to open it. In the Inspector you can enable Depth Texture.
+
+Note: If you are using both Stylized Fire FX I and II, import both assets before you apply the URP upgrade.
+
+---------------
+
+Need help with anything?
+
+See the FAQ or get in touch here:
+
+https://archanor.com/support.html
