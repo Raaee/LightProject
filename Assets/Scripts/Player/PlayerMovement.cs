8e7a187cbbc0b145cf885c541d066c6545943934
ally <roa.ayleen07@gmail.com>
2024-06-12 21:52:57 -0400

    Working on flipping character left and right

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using FunkyCode.Rendering.Day;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -15,6 +16,9 @@ public class PlayerMovement : MonoBehaviour
 
     private const float SMOOTH_TIME = 0.1f;
     private bool isCurrentlyMoving = false;
+    private bool facingRight = true;
+    private bool facingUp = false;
+    private bool facingDown = false;
 
     [HideInInspector] public UnityEvent OnPlayerMove;
     [HideInInspector] public UnityEvent OnPlayerStop;
@@ -38,13 +42,18 @@ public class PlayerMovement : MonoBehaviour
         moveInput = playerInput.movement.ReadValue<Vector2>();
         smoothedMovementInput = Vector2.SmoothDamp(smoothedMovementInput, moveInput, ref movementInputSmoothVelocity, SMOOTH_TIME);
         rb.velocity = smoothedMovementInput * moveSpeed;
+  
 
-        if(isCurrentlyMoving)
+
+        if (isCurrentlyMoving)
         {
             if (!IsPlayerMoving())
             {
+                ResetRotation();
                 OnPlayerStop?.Invoke();
                 isCurrentlyMoving = false;
+                
+
             }
            
         }
@@ -52,13 +61,85 @@ public class PlayerMovement : MonoBehaviour
         {
             if (IsPlayerMoving())
             {
+              
                 OnPlayerMove?.Invoke();
                 isCurrentlyMoving = true;
             }
         }
-      
-       
+
     }
+
+    private void Update()
+    {
+        FlipPlayer();
+    }
+
+
+
+    public void FlipPlayer()
+    {
+        
+        if (moveInput.y > 0 && facingUp)
+        {
+            //ResetRotation();
+            //RotatePlayerUp();
+        }
+        else if (moveInput.y < 0 && !facingUp)
+        {
+           // ResetRotation();
+            //FlipPlayerDown();
+        }
+       if (moveInput.x < 0 && facingRight)
+        {
+            ResetRotation();
+            FlipPlayerLeftNRight();
+        }
+       else if (moveInput.x > 0 && !facingRight)
+        {
+            ResetRotation();
+            FlipPlayerLeftNRight();
+        }
+
+        
+
+    }
+
+    public void FlipPlayerLeftNRight()
+    {
+        Vector2 playerScale = transform.localScale;
+        playerScale.x *= -1;
+        gameObject.transform.localScale = playerScale;
+
+        facingRight = !facingRight;
+    }
+
+    public void RotatePlayerUp()
+    {
+        /*Vector2 playerScale = transform.localScale;
+        playerScale.y *= -1;
+        gameObject.transform.localScale = playerScale;*/
+
+        transform.rotation = Quaternion.Euler(0, 0, 90);
+        facingUp = !facingUp;
+
+     
+    }
+
+    public void ResetRotation()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    public void FlipPlayerDown()
+    {
+        
+
+        transform.rotation = Quaternion.Euler(0, 0, -90);
+
+        facingUp = !facingUp;
+    }
+
+    
 
     public void AllowPlayerInputOnAnimationEnd()
     {
