a9f1b0ea2b36dfdeeeab75f912e2f73fb54451cc
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-07-10 18:58:23 -0400

    automatic portal sandbox configs

--- Assets/Scripts/Lock System/Portal.cs
+++ Assets/Scripts/Lock System/Portal.cs
@@ -9,16 +9,17 @@ public class Portal : MonoBehaviour
 {
     private Door door;
      [HideInInspector] public UnityEvent OnPlayerEntersPortal;
-     [SerializeField] private bool isGameplayLevel = false;
+      private bool isGameplayLevel = true;
      private float waitTime = 2.0f;
      
     private void Awake()
     {
         door = GetComponent<Door>();
-        if(isGameplayLevel)
-            Debug.LogWarning("This is a GAMEPLAY SCENE based on portal config." +
-                             " When player goes to portal it will increase currentLevel progress");
+        DetermineIfGameplayLevel();
+        
     }
+
+  
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -42,8 +43,15 @@ public class Portal : MonoBehaviour
         }
         else
         {
-            //show pause menu 
-            Debug.Log("This is a sandbox level. Still figuring out what to do. for now go to pause menu and go back to menu");
+            var pauseMenu = FindObjectOfType<PauseSystem>();
+            if (pauseMenu == null)
+            {
+                Debug.LogError("Theres no pause prefab in this scene!");
+
+                return;
+            } 
+            pauseMenu.PauseGame();
+            
         }
         Debug.Log("Attempting to stop audio atmospher");
 
@@ -58,5 +66,38 @@ public class Portal : MonoBehaviour
         yield return new WaitForSeconds(time);
         int index = ES3.Load(Utility.CURRENT_LEVEL_KEY, 0);
         SceneManager.LoadScene(LevelSelectDataHandler.Instance.gamePlayLevelElements[index].scenePath);
+    }
+    
+    private void DetermineIfGameplayLevel()
+    {
+        var pathOfThisScene = SceneManager.GetActiveScene().path;
+        if (LevelSelectDataHandler.Instance.gamePlayLevelElements.Count == 0)
+        {
+            Debug.Log("Did not start this scene from the main menu");
+            return;
+        }
+
+        foreach (var item in LevelSelectDataHandler.Instance.sandboxLevelElements)
+        {
+            if (item.scenePath == pathOfThisScene)
+            {
+                isGameplayLevel = false;
+                Debug.LogWarning("This is a SANDBOX SCENE" +
+                                 " When player goes to portal it will show pause menu");
+            }
+            
+        }
+        
+        foreach (var item in LevelSelectDataHandler.Instance.gamePlayLevelElements)
+        {
+            if (item.scenePath == pathOfThisScene)
+            {
+                Debug.LogWarning("This is a GAMEPLAY SCENE" +
+                                 " When player goes to portal it will increase currentLevel progress");
+            }
+            
+        }
+     
+      
     }
 }
