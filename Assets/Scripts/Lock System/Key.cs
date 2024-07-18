68d38390dd2734fc3236443ba1bb98a1e36b7c5f
Raaee <99899402+Raaee@users.noreply.github.com>
2024-07-17 20:36:36 -0400

    fixed key pick up bug

--- Assets/Scripts/Lock System/Key.cs
+++ Assets/Scripts/Lock System/Key.cs
@@ -8,7 +8,9 @@ public class Key : MonoBehaviour, IInteractable
     //Picking up the key 
     public void Interact()
     {
-        Inventory.instance.AddItem(this.gameObject.transform.parent.gameObject);
-        this.gameObject.GetComponentInParent<Collider2D>().enabled = false;
+        if (!Inventory.instance.inventory) {
+            Inventory.instance.AddItem(this.gameObject.transform.parent.gameObject);
+            this.gameObject.GetComponentInParent<Collider2D>().enabled = false;
+        }
     }
 }
