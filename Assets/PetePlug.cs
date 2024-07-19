80bae45df6c75fb1ed3c1778abcbf152cf2f6fb8
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-07-19 00:28:12 -0400

    add very serious shameless plug

---
+++ Assets/PetePlug.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetePlug : MonoBehaviour
+{
+    public void GoToLink()
+    {
+        Application.OpenURL("https://youtu.be/k6ykHhAGQ30");
+    }
+}
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test1 : MonoBehaviour
 {
    
 }
